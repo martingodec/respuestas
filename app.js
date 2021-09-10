53b4e42628e913da3a5945ca6f2cdf1fcb9a3d4e
Martin Godec <martingodec@hotmail.com>
2021-09-10 12:51:15 -0300

    Actualizacion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const area = document.getElementById("prueba");
 area.addEventListener("input", function () {
   const texto = document.getElementById("prueba").value;
 
-  //const objetos = {url : "@direcciones", msje: "La sucursal se encuentra en la calle San Jose 426 esquina Belgrano."}
+ 
 
   const replace = texto.replace(/@link/g, "https://estoesunlink.com");
 
