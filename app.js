fc425cae61a5698688a21ce7ddd403c581c98672
Martin Godec <martingodec@hotmail.com>
2021-09-10 15:16:32 -0300

    Actualizacion con varios tags

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const area = document.getElementById("prueba");
 
+//Recibo tags de una DB (ejemplo con objetos)
+const objetos = [{tag:"@hola", msje: "Hola todo bien? En que podemos ayudarle ?"}, {tag:"@stock", msje: "Nos queda disponible en ese número"}, {tag:"@direccion", msje: "La direccion es calle falsa 123"}]
+
 area.addEventListener("input", function () {
   const texto = document.getElementById("prueba").value;
 
- 
+    for (let i = 0; i < objetos.length; i++ ){
 
-  const replace = texto.replace(/@link/g, "https://estoesunlink.com");
+       if  (texto.includes(objetos[i].tag)){
+           console.log("Escribio la palabra " + objetos[i].tag)
 
-  document.getElementById("prueba").value = replace;
+           setTimeout(cambia , 200);
+        
+        
+        function cambia() {
+           const replace = texto.replace(objetos[i].tag, objetos[i].msje);
+
+           document.getElementById("prueba").value = replace;
+        }
+       }
+
+    }
+
+
+  
 });
